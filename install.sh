8da3060322c3db0268d228f55755e1fd71ca4982
VladMallory <vladmallory0321@gmail.com>
2026-07-02 21:56:42 +1000

    install.sh/cache: substituters передаются явно, таймаут 5с

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@ nix-shell -p git --run "git clone https://github.com/VladMallory/nix-config /tmp
 
 cd /tmp/nix-config
 
-nix --extra-experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./linux/disk-config.nix || true
+SUBSTITUTERS="https://cache.nixos.org https://mirror.sjtu.edu.cn/nix-channels/store https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+
+nix --extra-experimental-features "nix-command flakes" run github:nix-community/disko --option substituters "$SUBSTITUTERS" -- --mode disko ./linux/disk-config.nix || true
 
 ROOT_PART=$(lsblk -l -p -o name,partlabel | grep "disk-my-disk-root$" | awk '{print $1}')
 ESP_PART=$(lsblk -l -p -o name,partlabel | grep "disk-my-disk-ESP$" | awk '{print $1}')
@@ -32,4 +34,4 @@ swapon /mnt/.swapfile 2>/dev/null || true
 nixos-generate-config --no-filesystems --root /mnt
 cp /mnt/etc/nixos/hardware-configuration.nix .
 nix-shell -p git --run "git add hardware-configuration.nix"
-nixos-install --root /mnt --flake .#nixos-dev
+nixos-install --root /mnt --flake .#nixos-dev --option substituters "$SUBSTITUTERS" --option connect-timeout 5
